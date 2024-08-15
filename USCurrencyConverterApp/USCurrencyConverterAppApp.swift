f2aedb3ff4f1d35d002a1a9783375f3fa5ee2134
Charlie Kisylia <kisyliac@yahoo.com>
2024-08-15 09:59:47 -0700

    Initial Commit

---
+++ USCurrencyConverterApp/USCurrencyConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  USCurrencyConverterAppApp.swift
+//  USCurrencyConverterApp
+//
+//  Created by Charlie Kisylia on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct USCurrencyConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
